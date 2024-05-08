00f404a35c7f738dcf5064b19d979252fe9f96c8
zensi-korat <zensi@simformsolutions.com>
2024-05-08 19:08:59 +0530

    music app

--- music_app/src/main.js
+++ music_app/src/main.js
@@ -1,4 +1,6 @@
-import { createApp } from 'vue'
-import App from './App.vue'
+import { createApp } from "vue";
+import App from "./App.vue";
+import "./assets/base.css";
+import "./assets/main.css";
 
-createApp(App).mount('#app')
+createApp(App).mount("#app");
